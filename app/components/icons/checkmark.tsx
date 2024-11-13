62734838add54739d03ddbba748be2ff1f8d23c4
Sean McQuaid <sean@decrypteddesign.com>
2024-11-13 13:59:51 -0800

    feat: add checkmark icon component

---
+++ app/components/icons/checkmark.tsx
@@ -0,0 +1,21 @@
+import {SVGProps} from 'react'
+
+const Checkmark = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    stroke="currentColor"
+    strokeWidth="1"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+)
+export default Checkmark
